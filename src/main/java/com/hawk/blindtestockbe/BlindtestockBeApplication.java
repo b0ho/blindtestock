27367d0db5ed2c7bb7afc690589b0473c41993cb
b0ho <b0ho>
2024-10-25 10:16:53 +0900

    test init

---
+++ src/main/java/com/hawk/blindtestockbe/BlindtestockBeApplication.java
@@ -0,0 +1,13 @@
+package com.hawk.blindtestockbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindtestockBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindtestockBeApplication.class, args);
+    }
+
+}
